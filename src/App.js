c7f9efd66be85fb26a1293b6ba550272d0c8bc1a
Keongy <info.pichon@gmail.com>
2024-06-20 00:45:59 +0200

    some text modif

--- src/App.js
+++ src/App.js
@@ -7,40 +7,53 @@ function App() {
     <div className="App">
       <Container>
 
-        <div className="title">
+        <div>
           <h1>Développeur js</h1>
         </div>
 
 
         <div className="content">
 
-          <div className='name'>
-            <h4>Prénom: Olivier</h4>
-            <h4>Nom : Pichon</h4>
-            <h4>Expérience : 3 ans</h4>
-            <h4>Lieu : 06 Sophia-Antipolis</h4>
-            <h4>Disponibilité : Immédiate</h4>
+          <div className='profil'>
+            <h2 className='title'>Info perso</h2>
+
+            <div className="wrapper-block">
+              <div className="personnal-info">
+                <ul>
+                  <li><p>Prénom: <strong>Olivier</strong></p></li>
+                  <li><p>Nom : <strong>Pichon</strong></p></li>
+                  <li><p>Expérience : <strong>3 ans</strong></p></li>
+                  <li><p>Lieu :<strong>06 Sophia-Antipolis</strong></p></li>
+                  <li><p>Disponibilité : <strong>Immédiate</strong></p></li>
+                </ul>
+              </div>
+            </div>
           </div>
 
           <div className="separator" />
 
-          <div className="competences">
-            <h2>Compétences principales</h2>
-            <ul>
-              <li><p>JavaScript, TypeScript</p></li>
-              <li><p>React.js, Next.js, Vue.js</p></li>
-              <li><p>Node.js, Express, Firebase</p></li>
-              <li><p>MongoDB, Mongoose</p></li>
-            </ul>
+          <div className='profil'>
+            <h2 className='title'>Skills</h2>
+
+            <div className="wrapper-block">
+              <div className="personnal-info">
+                <ul>
+                  <li><p>Html - CSS - JavaScript - TypeScript</p></li>
+                  <li><p>React.js, Next.js, Vue.js</p></li>
+                  <li><p>Node.js - Express - MongoDB - Mongoose </p></li>
+                </ul>
+                <br />
+                <p>Autres : VPS - PWA - Wordpress...</p>
+              </div>
+            </div>
           </div>
 
+
         </div>
-
-
-      </Container>
+      </Container >
 
       <Particles id="tsparticles" />
-    </div>
+    </div >
   );
 }
 
